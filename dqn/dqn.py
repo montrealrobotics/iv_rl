ef831314b7cd7fc116285fc3ef987c25db69bcf1
kaustubh <kaustubh3095@gmail.com>
2024-01-27 14:05:01 -0500

    global step change

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -11,7 +11,7 @@ from collections import namedtuple, deque, Counter
 import wandb
 from utils import * 
 from .networks import *
-
+ 
 
 class DQNAgent():
     """Interacts with and learns from the environment."""
@@ -239,9 +239,9 @@ class DQNAgent():
             scores_window.append(score)        # save most recent score
             scores.append(score)               # save most recent score
             eps = max(eps_end, eps_decay*eps)  # decrease epsilon
-            wandb.log({"Moving Average Return/100episode": np.mean(scores_window)})
-            wandb.log({"Num terminations ": num_terminations})
-            wandb.log({"Episode": i_episode-1})
+            wandb.log({"Moving Average Return/100episode": np.mean(scores_window)}, global_step=i_episode)
+            wandb.log({"Num terminations ": num_terminations}, global_step=i_episode)
+            wandb.log({"Episode": i_episode-1}, global_step=i_episode)
 
             #if np.mean(self.test_scores[-100:]) >= self.opt.goal_score and flag:
             #    flag = 0 
