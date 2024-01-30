35768f83c8c96f98447c6204fefd4223f4133018
kaustubh <kaustubh3095@gmail.com>
2024-01-30 16:04:16 -0500

    pushing cs51

--- dqn/__init__.py
+++ dqn/__init__.py
@@ -1,4 +1,4 @@
-from .dqn import DQNAgent, LossAttDQN
+from .dqn import DQNAgent, LossAttDQN, C51
 from .mcdropDQN import MCDropDQN
 from .ensembleDQN import EnsembleDQN, MaskEnsembleDQN, RPFMaskEnsembleDQN, BootstrapDQN, RPFBootstrapDQN, Lakshminarayan, LakshmiBootstrapDQN
 from .iv_dqn import IV_DQN, IV_MaskEnsembleDQN, IV_LossAttDQN, IV_RPFMaskEnsembleDQN, IV_BootstrapDQN, IV_MCDropDQN, IV_RPFBootstrapDQN, IV_Lakshminarayan, IV_LakshmiBootstrapDQN
